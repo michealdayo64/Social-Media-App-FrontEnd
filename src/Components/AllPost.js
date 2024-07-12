4659bc04977a8b6a5974f0c872fbc7ccf251d8d0
michealdayo64 <omotoshomicheal93@gmail.com>
2024-07-12 06:27:46 +0100

    ok

--- src/Components/AllPost.js
+++ src/Components/AllPost.js
@@ -10,29 +10,38 @@ import Moment from "react-moment";
 import { useDispatch, useSelector } from "react-redux";
 
 function AllPost({ socialData, likePostBtn, userRepostBtn }) {
-  const divRefContainer = useRef()
+  const divRefContainer = useRef();
+  const authState = useSelector((state) => state.auth);
+  const socialState = useSelector((state) => state.social);
+  const log_user = socialState.logginUser;
+  const userData = authState.user;
+
   function getExtension(filename) {
     return filename?.split(/[#?]/)[0].split(".").pop().trim();
   }
 
-  const social = useSelector((state) => state.social);
-  const log_user = social.logginUser;
-
-  const createComment = () =>{
-    //console.log(divRefContainer.current.getAttribute("data-id"))
-  }
-
-  useEffect(() =>{
-    console.log(divRefContainer.current)
-  })
+  const createComment = (id) => {
+    //e.preventDefault();
+    const commentidBtn = [...document.querySelectorAll("#commentIdBtn")];
+    commentidBtn.forEach((mm) => {
+      var b = mm.getAttribute("data-id");
+      if (b === `${id}`) {
+        console.log(`${id}`);
+        var comment_textId = document.querySelector(`#commenttextid_${id}`);
+        comment_textId?.classList.add("comment-text2");
+        var comment_listId = document.querySelector(`#commentlistid_${id}`);
+        comment_listId?.classList.add("comment-list2");
+      }
+    });
+  };
 
   return (
     <div>
       {socialData &&
         socialData.map((soc) => {
           //console.log(soc.user_like_post)
-          const element = React.createRef()
-          divRefContainer.current[soc.pk] = element
+          //const element = React.createRef()
+          //divRefContainer.current[soc.pk] = element
           const {
             pk,
             user,
@@ -88,7 +97,11 @@ function AllPost({ socialData, likePostBtn, userRepostBtn }) {
                     </span>
                   </div>
                   <div className="bottom-comment">
-                    <div ref={element} onClick={createComment} id="commentIdBtn" data-id={pk}>
+                    <div
+                      onClick={() => createComment(pk)}
+                      id="commentIdBtn"
+                      data-id={pk}
+                    >
                       <TfiCommentAlt className="icon-color" />
                       <span className="comment">
                         {comments.length} Commnets
@@ -102,6 +115,37 @@ function AllPost({ socialData, likePostBtn, userRepostBtn }) {
                     </div>
                   </div>
                 </div>
+                <div className="comment-text1" id={`commenttextid_${pk}`}>
+                  <img src={userData?.profile_pic} alt="prof-pic" />
+
+                  <form method="post" onSubmit={null}>
+                    <input type="text" placeholder="Add a comment..." />
+                  </form>
+                </div>
+                {comments &&
+                  comments.map((com) => {
+                    return (
+                      <div className="comment-list1" id={`commentlistid_${pk}`} key={com.pk}>
+                        <div className="comment-list-top">
+                          <img src={com.user?.profile_image} alt="proc-img" />
+                          <div className="comment-card">
+                            <div className="card-header">
+                              <a>{com.user?.username}</a>
+                              <span>
+                                4h <a>...</a>
+                              </span>
+                            </div>
+                            <div className="card-body">
+                              <p>{com.comment}</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="like-reply">
+                          <a>Like</a> <span>|</span> <a>Reply</a>
+                        </div>
+                      </div>
+                    );
+                  })}
               </div>
             </div>
           );

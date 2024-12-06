7fb722a94331138c7186543cb9390b4d64c6af3d
michealdayo64 <omotoshomicheal93@gmail.com>
2024-12-06 03:50:12 +0100

    ok

--- src/Components/ChatModal.js
+++ src/Components/ChatModal.js
@@ -11,12 +11,12 @@ function ChatModal() {
     useGlobalContext();
   const all = useSelector((state) => state.friend);
   const authState = useSelector((state) => state.auth);
-  const privateChatState = useSelector((state) => state.private_chat)
+  const privateChatState = useSelector((state) => state.private_chat);
   const dispatch = useDispatch();
   const allUsers = all.users;
-  const privateChatFriends = privateChatState.chatFriends
+  const privateChatFriends = privateChatState.chatFriends;
 
-  console.log(privateChatFriends)
+  console.log(privateChatFriends);
 
   return (
     <div
@@ -37,15 +37,23 @@ function ChatModal() {
               {privateChatFriends &&
                 privateChatFriends.map((user) => {
                   return (
-                    <div key={user.friend.pk}> 
+                    <div key={user.friend.pk}>
                       <div
-                        className="content-image-chat"
+                        className="content-image-chat-space"
                         onClick={openPrivateChatMessage}
                       >
-                        <img src={user.friend.profile_image} alt="chatimage" />
-                        <div className="content-chat">
-                          <span className="name">{user.friend.username}</span>
-                          <span className="mychat">hhshhhahajahagaggaga</span>
+                        <div className="content-image-chat">
+                          <img
+                            src={user.friend.profile_image}
+                            alt="chatimage"
+                          />
+                          <div className="content-chat">
+                            <span className="name">{user.friend.username}</span>
+                            <span className="mychat">hhshhhahajahagaggaga</span>
+                          </div>
+                        </div>
+                        <div className="chat-time">
+                          <span>2:44</span>
                         </div>
                       </div>
                       <hr></hr>
@@ -55,31 +63,31 @@ function ChatModal() {
             </div>
           </div>
         ) : (
-          <div>
+          <div className="private-chat-header-message-space">
             <div className="private-chat-header-message">
               <div className="head-chat-message">
-                <IoIosArrowBack className="arrow" onClick={openPrivateChatMessage} />
+                <IoIosArrowBack
+                  className="arrow"
+                  onClick={openPrivateChatMessage}
+                />
                 <img src={chatimage} alt="hdhdhd" />
               </div>
               <span>Micheal</span>
-              <span className="head-chat-message-icon"><SlOptionsVertical /></span>
+              <span className="head-chat-message-icon">
+                <SlOptionsVertical />
+              </span>
             </div>
-            <p>ncncincniiwincwc</p>
-            <p>ncncincniiwincwc</p>
-            <p>ncncincniiwincwc</p>
-            <p>ncncincniiwincwc</p>
-            <p>ncncincniiwincwc</p>
-            <p>ncncincniiwincwc</p>
-            <p>ncncincniiwincwc</p>
-            <p>ncncincniiwincwc</p>
-            <p>ncncincniiwincwc</p>
-
-            <p>ncncincniiwincwc</p>
-
-            <p>ncncincniiwincwc</p>
-            <p>ncncincniiwincwc</p>
-
-            <p>ncncincniiwincwc</p>
+            <div className="message-container">
+              <p>hello</p>
+              <p>hello</p>
+              <p>hello</p>
+              <p>hello</p>
+              <p>hello</p>
+            </div>
+            <div className="message-input">
+              <input type="text" placeholder="Enter Message" />
+              <button>hello</button>
+            </div>
           </div>
         )}
       </div>
